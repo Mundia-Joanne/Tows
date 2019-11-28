f211bf65c76a91448a9c3a5879e47553ff4a17ed
Joanne-Mundia <36207969+Joanne-Mundia@users.noreply.github.com>
2019-11-28 14:33:15 +0300

    First Commit

---
+++ app/src/main/java/com/example/tows/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.tows;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE =10001;
+    List<AuthUI.IdpConfig> providers;
+    Button sign_out;
+    private int requestCode;
+
+    private Button btn_garage,btn_tows;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_garage = (Button) findViewById(R.id.btn_garage);
+        btn_garage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapsActivity();
+            }
+            public void openMapsActivity(){
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+
+
+        sign_out = (Button)findViewById(R.id.sign_out);
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                sign_out.setEnabled(true);
+                                showSignInOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        //init providers
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),//Email Builder
+                new AuthUI.IdpConfig.PhoneBuilder().build(),//Phone Builder
+               new AuthUI.IdpConfig.FacebookBuilder().build(),//Facebook Builder
+                new AuthUI.IdpConfig.GoogleBuilder().build()//Google Builder
+        );
+
+        showSignInOptions();
+
+
+
+    }
+
+  /*  public void openMapsActivity(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }*/
+
+
+    private void showSignInOptions() {
+       startActivityForResult(
+               AuthUI.getInstance().createSignInIntentBuilder()
+               .setAvailableProviders(providers)
+               .setTheme(R.style.MyTheme)
+               .build(),MY_REQUEST_CODE
+       );
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        if(requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode== RESULT_OK)
+            {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
+                sign_out.setEnabled(true);
+
+            }
+            else
+            {
+                Toast.makeText(this, ""+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
+
